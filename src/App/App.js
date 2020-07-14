2475df750ebed6bdf24973b75b8160aed62b9a8c
Conner McCabe <conner.p.mccabe@gmail.com>
2020-07-14 13:21:31 -0500

    starting navbar

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from '../Components/NavBar/NavBar';
+import News from '../Components/News/News';
+import Product from '../Components/Products/Product';
+import Footer from '../Components/Footer/Footer';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        start up
+        <NavBar />
+        <News />
+        <Product />
+        <Footer />
       </React.Fragment>
     )
   }
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -5,7 +5,16 @@ class NavBar extends Component {
     render() {
         return (
             <div className="NavBar">
-                <h1>NavBar test</h1>
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href="https://github.com/ConnerMcCabe">thing1</a></li>
+                            <li><a href="https://github.com/ConnerMcCabe">thing2</a></li>
+                            <li><a href="https://github.com/ConnerMcCabe">thing3</a></li>
+                        </ul>
+                    </nav>
+                    <a href="cta"><button>Cart</button></a>
+                </header>
             </div>
         );
     }
