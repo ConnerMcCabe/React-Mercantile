03febed2a938167e925c1072c83b530d241bfa5a
Conner McCabe <conner.p.mccabe@gmail.com>
2020-07-14 12:32:05 -0500

    getting started

--- src/App/App.js
+++ src/App/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import NavBar from '../Components/NavBar/NavBar.js';
-import Product from '../Components/Products/Product.js';
-import News from '../Components/News/News.js';
-import Footer from '../Components/Footer/Footer.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <Product />
-      <News />
-      <Footer />
+      <div className="col-6">
+        <span><i>i.fas.fa-home</i></span>
+      </div>
+      
     </div>
   );
 }
